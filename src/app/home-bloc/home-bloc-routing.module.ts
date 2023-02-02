005ea3b577322688f03a89aea12ae8a0c9bd6340
OsmanFerrer27 <91578666+OsmanFerrer27@users.noreply.github.com>
2023-02-02 12:52:00 -0400

    Add files via upload

--- src/app/home-bloc/home-bloc-routing.module.ts
+++ src/app/home-bloc/home-bloc-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
 import { HomeBlocPage } from './home-bloc.page';
+import { Component } from '@angular/core'; 
+import { AlertController } from '@ionic/angular';
 
 const routes: Routes = [
   {
