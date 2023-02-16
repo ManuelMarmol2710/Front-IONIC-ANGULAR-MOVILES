23e81ea1519fda12abef3839c9d993817412d011
ManuelMarmol2710 <92897105+ManuelMarmol2710@users.noreply.github.com>
2023-02-16 16:15:05 -0400

    Add files via upload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,6 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
-  {
     path: '',
     redirectTo: 'signin',
     pathMatch: 'full'
@@ -34,10 +30,6 @@ const routes: Routes = [
   {
     path: 'deleteprofile',
     loadChildren: () => import('./deleteprofile/deleteprofile.module').then( m => m.DeleteprofilePageModule)
-  },
-  {
-    path: 'ver-notas',
-    loadChildren: () => import('./ver-notas/ver-notas.module').then( m => m.VerNotasPageModule)
   },
   {
     path: 'ver-collections',
